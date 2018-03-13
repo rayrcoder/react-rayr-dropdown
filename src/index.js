1650c2b592abccd30141262b33cf8c6cad3435d9
luokaihui <luokaihui_i@didichuxing.com>
2018-03-13 11:33:41 +0800

    build v0.0.7

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import RayrDropdown from './RayrDropdown.js';
-import RayrDropdown_Select from './RayrDropdown_Select.js';
-import Grid from './Grid.js';
-import Game from './Game.js';
 
 import Dropdown from './Dropdown.js';
 import DropdownHeader from './DropdownHeader.js';
@@ -12,4 +9,4 @@ Dropdown.Header = DropdownHeader;
 Dropdown.Body = DropdownBody;
 Dropdown.List = DropdownList;
 
-export {RayrDropdown, RayrDropdown_Select, Grid, Game, Dropdown};
+export {RayrDropdown, Dropdown};
